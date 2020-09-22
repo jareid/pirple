ac284194d0e78105cfae43a5cbc2ff21705311a7
Jamie Reid <jamie.reid@gmx.com>
2020-09-22 13:51:02 +0200

    Remove non numerical attributes as per homework instructions

--- C#/HW1/Homework1/Homework1/animal.cs
+++ C#/HW1/Homework1/Homework1/animal.cs
@@ -17,30 +17,24 @@ namespace Rextester
          */
         public static void Main(string[] args)
         {
-            string animalType = "Dog";                                          // The animal Type
-            string animalName = "Cooper";                                       // The animal name
             float weightInKilos = 17.5f;                                        // The wgight of the animal in kilograms
             double weightInPounds = 38.5808576543;                               // The weight of the animal converted to kilograms
             int heightInCM = 75;                                                // The height oif the animal in centrimetres
             float heightInFeet = 2.46063f;                                      // The height of the animal converted to feet
             int numberOfTeeth = 42;                                             // The number of teeth the animal has                                        
             bool isPredator = true;                                             // boiolean if the animal is a predator or not
-            string animalPrey = "rabbits";                                      // string describing the prey of the animal
             uint averageSpeed = 20;                                             // The speed of the animal in miles per hour
             short averageSpeedMetres = 32186;                                   // The speed of the animal in metres per hour
             ushort averageAgeYears = 13;                                        // The average age of the animal in years
             long averageAgeSeconds = 409968000;                                 // The average age of the animal in seconds
 
             // Output the animals details to the console.
-            Console.WriteLine(animalType);
-            Console.WriteLine(animalName);
             Console.WriteLine(weightInKilos);
             Console.WriteLine(weightInPounds);
             Console.WriteLine(heightInCM);
             Console.WriteLine(heightInFeet);
             Console.WriteLine(numberOfTeeth);
             Console.WriteLine(isPredator);
-            Console.WriteLine(animalPrey);
             Console.WriteLine(averageSpeed);
             Console.WriteLine(averageSpeedMetres);
             Console.WriteLine(averageAgeYears);
