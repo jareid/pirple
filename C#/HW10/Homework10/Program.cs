80b700775757183df755cba187f361cb68302cc5
Jamie Reid <jamie.reid@gmx.com>
2021-01-16 10:32:55 +0100

    Add new courses

---
+++ C#/HW10/Homework10/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Homework10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string csvFilename = "fifa2019.csv";
+            string tsvFilename = "fifa2020.tsv";
+
+            StringBuilder sB = new StringBuilder();
+            try
+            {
+                using (System.IO.StreamReader rdr = new System.IO.StreamReader(Path.Combine(dir, csvFilename)))
+                {
+                    int counter = 0;
+                    string line;
+                    while ((line = rdr.ReadLine()) != null)
+                    {
+                        string[] arr = line.Split(',');
+                        var csvLine = string.Join("\t", arr);
+                        sB.Append(csvLine).Append("\n");
+                        counter++;
+                    }
+                }
+                try
+                {
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(dir, tsvFilename)))
+                    {
+                        outputFile.Write(sB.ToString());
+                    }
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine("Exiting as unable to write to new file, see below");
+                    Console.WriteLine(ex); // Write error
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Exiting as File not found, see below");
+                Console.WriteLine(ex); // Write error
+            }
+}
+
+    }
+}
---
+++ JS_ES6/song.js
@@ -0,0 +1,23 @@
+// The following lines create variables containing metadata about a song
+Artist = "Tupac"            // The artist (string)
+Genre = "Hip-Hop"           // The genre  (string)
+Title = "Life Goes On"      // The song title (string)
+DurationInSeconds = 301     // The length of  the song in seconds (integer)
+Album = "All Eyez On Me"    // The album name (string)
+AlbumArtist = "Tupac"       // The album artist (string)
+FileSizeMB = 11.5           // the filesize in megabytes (float)
+Year = 2001                 // the year the song came out (integer)
+Rating = 4.5                // the rating out of 5 (float)
+
+
+// The following lines console.log out the created variables
+
+console.log(Artist)
+console.log(Genre)
+console.log(Title)
+console.log(DurationInSeconds)
+console.log(Album)
+console.log(AlbumArtist)
+console.log(FileSizeMB)
+console.log(Year)
+console.log(Rating)
